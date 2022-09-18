a26fea0010f42bb5b30eecde83632980acb8bc1a
aerocloud <aerocloud27@gmail.com>
2022-09-18 21:30:27 +0200

    Fixed an error in 8ball response sentences

--- src/types/8ball.ts
+++ src/types/8ball.ts
@@ -13,7 +13,7 @@ export const eightBallSentencesEng = [
 	'It\'s better that you don\'t know right now',
 	'Perhaps...',
 	'Even I can\'t give you the answer to that question',
-	'"I prefer really not to speak. If I speak, I\'m in big trouble. And I don\t want to be in big trouble"',
+	'I prefer really not to speak. If I speak, I\'m in big trouble. And I don\t want to be in big trouble',
 	// Negative
 	'No way',
 	'Well, yes but actually no',
@@ -40,8 +40,8 @@ export const eightBallSentencesFr = [
 	// Indécis
 	'Mieux vaut pas que tu saches pour le moment',
 	'Peut-être...',
-	'Even I can\'t give you the answer to that',
-	'"I prefer really not to speak. If I speak, I\'m in big trouble. And I don\t want to be in big trouble"',
+	'Même moi je ne saurais pas répondre à ça',
+	'Je préfère vraiment ne rien dire. Si je parle je vais avoir de gros problèmes. Et je ne veux pas avoir de gros problèmes',
 	// Négatif
 	'Non, pas moyen',
 	'Oui... Euh en fait non, vraiment pas',
