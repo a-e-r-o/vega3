f84def4096a608fded194e9a564f57a8ee427903
aerocloud <aerocloud27@gmail.com>
2022-10-16 13:42:44 +0200

    disabled unfinished pref command

--- src/commands/vega.prefs.ts
+++ src/commands/vega.prefs.ts
@@ -1,6 +1,7 @@
 import { ctx, Cmd, CmdCall } from '../mod.ts'
 
 export const prefs: Cmd = {
+	disabled: true,
 	aliases: ['pref', 'prefs', 'param', 'params'],
 	execute: (call: CmdCall) => {
 		ctx.prefsService.updatePrefs(call)
