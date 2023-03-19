381b7dad2b482cb474e6d5fe1063458e6ae5b9e2
aerocloud <aerocloud27@gmail.com>
2023-03-19 20:37:10 +0100

    Handle exceptions

--- src/commands/configuration/triggers.ts
+++ src/commands/configuration/triggers.ts
@@ -1,16 +1,16 @@
-import { ctx, Cmd, CmdCall, langOptions, CmdTags } from '../../mod.ts'
+import { ctx, Cmd, CmdCall, CmdTags } from '../../mod.ts'
 
 /**
  * List all triggers for the current guild, with their IDs (needed to delete them)
  */
 export const listTrigger: Cmd = {
 	tags: CmdTags.DisabledInDm + CmdTags.BotAdminRequired,
-	aliases: ['triggerlist', 'trigger'],
+	aliases: ['triggerlist', 'triggers'],
 	execute: (call: CmdCall) => {
 		const list = ctx.guildSettingsService.triggerList(call.msg.guildId!)
 		let res = ''
 		for (let i = 0; i < list.length; i++) {
-			res += `ID : ${i} => ${list[i].regex}\n`
+			res += `\`ID\` : ${i} => \`${list[i].regex}\`\n`
 		}
 		res = res == '' ? 'No triggers created for this Discord Server' : res
 		return res
@@ -29,8 +29,19 @@ export const addTrigger: Cmd = {
 
 		if (options.length < 2)
 			throw "Missing arguments. Syntax is +trigger [regex] ; [response] ; (optional)[regex options]"
+		
+		const regex = options[0].replaceAll(/^`|`$/gi, '')
+		const regexOptions = options[2]
+		const response = options[1]
 
-		return ctx.guildSettingsService.addTrigger(call.msg.guildId!, options[0], options[1], options[2])
+		try {
+			''.match(new RegExp(regex, regexOptions))
+		}
+		catch (error) {
+			throw `Malformed error \`${error}\``
+		}
+
+		return ctx.guildSettingsService.addTrigger(call.msg.guildId!, regex, response, regexOptions)
 	}
 }
 
--- src/helpers/triggers.ts
+++ src/helpers/triggers.ts
@@ -1,10 +1,22 @@
-import { GuildSettings, Message } from "../mod.ts"
+import { GuildSettings, Message, vegaLog } from "../mod.ts"
 
 export function checkTriggers(msg: Message, guildSettings: GuildSettings){
-	// Find if the message matches any trigger word pattern
-	const foundPattern = guildSettings.triggers.find(
-		pattern => msg.content.match(new RegExp(pattern.regex, pattern.regexOptions))
-	)
+	let foundPattern = undefined
+
+	// Find if the message matches any trigger pattern
+	for (let i = 0; i < guildSettings.triggers.length; i++) {
+		const pattern = guildSettings.triggers[i];
+		try {
+			if (msg.content.match(new RegExp(pattern.regex, pattern.regexOptions))){
+				foundPattern = pattern
+				break
+			}
+		} 
+		catch {
+			vegaLog(`Malformed regex /${pattern.regex}/${pattern.regexOptions} on server ${guildSettings.guildId}`)
+		}
+	}
+
 	// If no pattern found, exit
 	if (!foundPattern)
 		return
